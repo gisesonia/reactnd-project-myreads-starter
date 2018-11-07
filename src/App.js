dc9f1828b97aad6cd99d8fdb7b3992c0d9ed7e72
gisesonia <gisellytoledo@bol.com.br>
2018-11-07 09:13:52 -0200

    Update App.js
    Correção do html

--- src/App.js
+++ src/App.js
@@ -4,35 +4,29 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    
   }
 
   render() {
     return (
       <div className="app">
-        <div className="list-books">
+         <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
               <div>
+      
                
-               
-        
               </div>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
-        )}
-      </div>
-    )
+       
+      </div>  
+    )}
+}
 
 export default BooksApp
