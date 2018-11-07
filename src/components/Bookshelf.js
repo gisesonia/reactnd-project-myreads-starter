cf45c6fc78a2c796a688fe9d4f41ad8bac9caae9
Gisele <gisesonia@gmail.com>
2018-11-06 23:16:46 -0200

    Separação do html em componentes

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,11 @@
+<div className="bookshelf">
+    <h2 className="bookshelf-title">Currently Reading</h2>
+    <div className="bookshelf-books">
+        <ol className="books-grid">
+            <li>
+
+            </li>
+
+        </ol>
+    </div>
+</div>
