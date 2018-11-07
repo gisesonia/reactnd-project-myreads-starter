70e0f0dfd2138327ed32549a0e5d251abf8b09aa
gisesonia <gisellytoledo@bol.com.br>
2018-11-07 11:50:08 -0200

    Classe e exportação do componente BookShelf

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,11 +1,20 @@
-<div className="bookshelf">
-    <h2 className="bookshelf-title">Currently Reading</h2>
-    <div className="bookshelf-books">
-        <ol className="books-grid">
-            <li>
+import React, { Component } from 'react'
+import * as Book from './components/Book'
 
-            </li>
-
-        </ol>
-    </div>
-</div>
+class BookShelf extends Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book />
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+export default BookShelf
