6b21c4d030c8ec34766a5bf4ffe1087ccfe91b6c
gisesonia <gisellytoledo@bol.com.br>
2018-11-08 11:23:20 -0200

    Correção props e map

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class BookShelf extends Component {
-   constructor(props) {
-   super(props);
- }
-  
-    render() {
-      
+     render() {
+      console.log(this.props)
       const { books } = this.props;
       
         return (          
        <div className="bookshelf">
-      {this.props.books.map((book) => (
-        <div>
+      {books.map((book) => (
+        <div key={book.id}>
           <h2 className="bookshelf-title">Currently Reading</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         <li>
-                            <Book />
+                            <Book shelf={this.props.books} />
                         </li>
                     </ol>
           </div>
