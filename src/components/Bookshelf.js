ad76122cb5eebb9d897b53715be850468bfb7174
gisesonia <gisellytoledo@bol.com.br>
2018-11-08 18:02:24 -0200

    Inserção da função que muda de estante

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import Book from './Book'
 
-
 class BookShelf extends Component {
-  
-     render() {
-       //const { livros } = this.props;
-      console.log(this.props)
-       let filteredBooks = this.props.livros.filter(book => book.shelf === this.props.value);
-        return (          
-       <div className="bookshelf">       
-          <h2 className="bookshelf-title">{this.props.titulo}</h2>
+
+    render() {
+        //const { livros } = this.props;
+        console.log(this.props)
+        let filteredBooks = this.props.books.filter(book => book.shelf === this.props.value);
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.titulo}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {filteredBooks.map(book => (
                             <li key={book.id}>
-                                <Book book={book} />
+                                <Book 
+				  book={book} 
+				  changeShelf={this.props.changeShelf}
+				/>
                             </li>
                         ))}
                     </ol>
-          </div>
-      
-          </div>)
-	}
+                </div>
+            </div>)
+    }
 }
-          
+
 export default BookShelf
