e550b784343f99433f9e76fcdb3e902ca384b297
gisesonia <gisellytoledo@bol.com.br>
2019-02-11 23:02:38 -0200

    Atualizando as estantes (shelf) na busca

--- src/App.js
+++ src/App.js
@@ -1,54 +1,100 @@
-import React from 'react'
-import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import * as BooksAPI from './BooksAPI'
-import BookShelf from './components/BookShelf'
-import SearchBar from './components/SearchBar'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import BookShelf from "./components/BookShelf";
+import SearchBar from "./components/SearchBar";
+import "./App.css";
 
-class BooksApp extends React.Component {
-
-  state = {
-    books: []
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateQuery = event => {
+      this.setState({ query: event.target.value });
+      this.searchBook(event.target.value);
+    };
   }
+  state = {
+    books: [],
+    foundBooks: [],
+    query: ""
+  };
 
   componentDidMount() {
-    BooksAPI.getAll()
-      .then((books) => {
-        //console.log(books)
-        this.setState(() => ({
-          books
-        }))
-      })
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({
+        books
+      }));
+      
+    });
+
+    this.searchBook = query => {
+      BooksAPI.search(query, 20).then(response => {
+        if (response.error) {
+          this.setState({
+            foundBooks: []
+          });
+        } else {
+          this.setState({ foundBooks: response });
+        }        
+      });
+      this.foundBooksupdate(this.state.books, this.state.books.shelf); 
+    };
   }
+
+  foundBooksupdate = (books, shelf) => {
+    const verifiedBooks = books.map(book => {      
+      if(this.state.foundBooks.length > 0){
+      this.state.foundBooks.forEach(bookShelf => {
+        if (book.id === bookShelf.id) {
+          book.shelf = bookShelf.shelf;
+        }else if(book.shelf === undefined) {
+          book.shelf = "none"; 
+          bookShelf.shelf = "none"
+        }
+      });     
+    }
+    return book;
+    });
+    this.setState({
+      foundBooks: verifiedBooks
+    });
+  };
+
+
+
   newShelf = (newBook, shelf) => {
-    BooksAPI.update(newBook, shelf).then(response => {
+    BooksAPI.update(newBook, shelf).then(response => {      
       newBook.shelf = shelf;
+      
       this.setState(prevState => ({
         books: prevState.books
           .filter(book => book.id !== newBook.id)
           .concat(newBook)
       }));
+     
     });
+    
   };
 
   render() {
+    //console.log(this.props.booksOnShelf)
     const bookshelf = [
       {
-        value: 'currentlyReading',
-        title: 'Currently Reading',
+        value: "currentlyReading",
+        title: "Currently Reading",
         id: 1
       },
       {
-        value: 'wantToRead',
-        title: 'Want to Read',
+        value: "wantToRead",
+        title: "Want to Read",
         id: 2
       },
       {
-        value: 'read',
-        title: 'Read',
+        value: "read",
+        title: "Read",
         id: 3
       }
-    ]
+    ];
     return (
       <div className="app">
         <div className="list-books">
@@ -56,25 +102,44 @@ class BooksApp extends React.Component {
             <h1>MyReads</h1>
           </div>
           <Router>
-            <Route exact path='/' render={() => (
-              <div className="list-books-content">
-                {bookshelf.map(shelf =>
-                    <BookShelf key={shelf.id}
-                      titulo={shelf.title}
-                      value={shelf.value}
-                      books={this.state.books}
-                      newShelf={this.newShelf}
-                  />)}
-              </div>
-            )} />
-          </Router>
-          <Router>
-            <Route path='/create' component={SearchBar} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <div className="list-books-content">
+                    {bookshelf.map(shelf => (
+                      <BookShelf
+                        key={shelf.id}
+                        titulo={shelf.title}
+                        value={shelf.value}
+                        books={this.state.books}
+                        newShelf={this.newShelf}
+                      />
+                    ))}
+                  </div>
+                )}
+              />
+              <Route
+                exact
+                path="/search"
+                render={() => (
+                  <SearchBar
+                    foundBooks={this.state.foundBooks}
+                    booksShelf={this.state.books}
+                    query={this.state.query}
+                    onUpdateQuery={this.updateQuery}
+                    newShelf={this.newShelf}
+                  />
+                )}
+              />
+            </Switch>
           </Router>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default App;
+
