9e58caf8c890674cc209ba2c6f0f8ac9b6c87846
gisesonia <gisellytoledo@bol.com.br>
2019-02-11 23:05:42 -0200

    Funções de atualização da busca

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Book from "./Book";
 
-
-class SearchBar extends React.Component {
+class SearchBar extends Component {
 
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link
-            className="close-search"
-            to='/'>
+          <Link className="close-search" to="/">
             Close
-                </Link>
+          </Link>
           <div className="search-books-input-wrapper">
-            {
-
-            }
-            <input type="text" placeholder="Search by title or author" />
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.props.query}
+              onChange={this.props.onUpdateQuery}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            <div>
+              {this.props.foundBooks.length > 0 ? (
+                this.props.foundBooks.map(book => (
+                  <li key={book.id}>
+                    <Book book={book} newShelf={this.props.newShelf} />
+                  </li>
+                ))
+              ) : (
+                <div>Nenhum livro encontrado...</div>
+              )}
+            </div>
+          </ol>
         </div>
       </div>
-    )
+    );
   }
 }
 export default SearchBar;
